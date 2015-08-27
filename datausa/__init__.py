a9186be33f753f1b80a79ea3020789aa5f172f82
Jon Speiser <jspeis@gmail.com>
2015-08-27 12:58:03 -0400

    cleaning up old references

--- datausa/__init__.py
+++ datausa/__init__.py
@@ -5,9 +5,7 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 from datausa.attrs.views import mod as attrs_module
-from datausa.ipeds.views import mod as ipeds_module
 from datausa.core.views import mod as core_module
 
 app.register_blueprint(attrs_module)
-app.register_blueprint(ipeds_module)
 app.register_blueprint(core_module)
