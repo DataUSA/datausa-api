2e1fde492706b0cde98048a41dd01dcd138e4454
Administrator <admin@example.com>
2015-09-09 14:58:08 -0400

    Allowing CORS

--- datausa/__init__.py
+++ datausa/__init__.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask
 from flask.ext.compress import Compress
+from flask.ext.cors import CORS
 
 app = Flask(__name__)
 app.config.from_object('config')
 Compress(app)
+CORS(app)
 
 from datausa.attrs.views import mod as attrs_module
 from datausa.core.views import mod as core_module
