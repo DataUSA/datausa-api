4b614e6c3bb76ed35ea6113f0d687afd485fe9a1
jspeis <jspeis@gmail.com>
2015-09-21 11:52:10 -0400

    Adding wage_bin to attrs. Adding endpoint to list supported attributes

--- datausa/attrs/views.py
+++ datausa/attrs/views.py
@@ -4,14 +4,16 @@ from flask import Blueprint, request, jsonify
 mod = Blueprint('attrs', __name__, url_prefix='/attrs')
 from datausa.attrs.models import Cip, Naics, University, Soc, Degree
 from datausa.attrs.models import Skill, Sector, Geo
-from datausa.attrs.models import PumsDegree, PumsNaics, PumsRace, PumsSex, PumsBirthplace
+from datausa.attrs.models import PumsDegree, PumsNaics, PumsRace
+from datausa.attrs.models import PumsWage, PumsSex, PumsBirthplace
 
 attr_map = {"soc": Soc, "naics" : Naics, "cip": Cip,
             "geo": Geo, "university": University, "degree": Degree,
             "skill": Skill, "sector": Sector,
             "pums_degree": PumsDegree,
             "race": PumsRace, "sex": PumsSex,
-            "birthplace": PumsBirthplace }
+            "birthplace": PumsBirthplace,
+            "wage_bin": PumsWage }
 
 def show_attrs(attr_obj):
     attrs = attr_obj.query.all()
@@ -49,4 +51,8 @@ def attrs_by_id(kind, attr_id):
         tmp = aid_obj.serialize()
         return jsonify(data=[tmp.values()], headers=tmp.keys())
     raise Exception("Invalid attribute type.")
+
+@mod.route("/list")
+def attrs_list():
+    return jsonify(data=attr_map.keys())
 
