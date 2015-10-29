e4ccd58a5ed885d346e1ee720351d83beca6cc3b
jspeis <jspeis@gmail.com>
2015-10-29 17:12:32 -0400

    adding chr crosswalker

--- datausa/core/crosswalker.py
+++ datausa/core/crosswalker.py
@@ -2,19 +2,22 @@ from datausa.attrs.models import PumsNaicsCrosswalk, IoCodeCrosswalk
 from datausa.attrs.models import GeoContainment
 from datausa.attrs.consts import OR
 from datausa import cache
+from sqlalchemy import or_
+
 
 @cache.memoize()
 def pums_naics_mapping():
     '''Make a dictionary that maps naics codes to PUMS
     NAICS codes'''
     all_objs = PumsNaicsCrosswalk.query.all()
-    return {obj.naics : obj.pums_naics for obj in all_objs}
+    return {obj.naics: obj.pums_naics for obj in all_objs}
+
 
 @cache.memoize()
 def iocode_mapping():
     '''Make a dictionary that maps naics codes to iocodes'''
     all_objs = IoCodeCrosswalk.query.all()
-    return {obj.naics : obj.iocode for obj in all_objs}
+    return {obj.naics: obj.iocode for obj in all_objs}
 
 
 def pums_parent_puma(geo_id):
@@ -39,6 +42,22 @@ def pums_parent_puma(geo_id):
             raise Exception("Not yet implemented")
     return geo_id
 
+def chr_parents(geo_id):
+    '''CHR data'''
+    needs_crosswalk = ["160", "310", "795"]
+    prefix = geo_id[:3]
+    if prefix in needs_crosswalk:
+        filters = [
+            GeoContainment.child_geoid == geo_id,
+            or_(GeoContainment.parent_geoid.startswith("040"),
+                GeoContainment.parent_geoid.startswith("050"))
+        ]
+        qry = GeoContainment.query.filter(*filters)
+        qry = qry.order_by(GeoContainment.percent_covered.desc())
+        geo_contain = qry.first()
+        if geo_contain:
+            return geo_contain.parent_geoid
+    return geo_id
 
 def crosswalk(table, api_obj):
     '''Given a table and an API object, determine if any crosswalks need
@@ -47,7 +66,9 @@ def crosswalk(table, api_obj):
         {"column": "industry_iocode", "schema": "bea", "mapping": iocode_map},
         {"column": "naics", "schema": "pums_beta", "mapping": naics_map},
         {"column": "cip", "schema": "pums_beta", "mapping": lambda x: x[:2]},
-        {"column": "geo", "schema": "pums_beta", "mapping": pums_parent_puma}
+        {"column": "geo", "schema": "pums_beta", "mapping": pums_parent_puma},
+        {"column": "geo", "schema": "chr", "mapping": chr_parents}
+
     ]
 
     for rcrosswalk in registered_crosswalks:
