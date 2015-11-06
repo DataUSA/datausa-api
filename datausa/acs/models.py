cf8b6d2636ce142b6948a1cc93595ee7cbf8ba58
jspeis <jspeis@gmail.com>
2015-11-06 14:28:16 -0500

    updating sumlevels

--- datausa/acs/models.py
+++ datausa/acs/models.py
@@ -71,7 +71,7 @@ class Acs5_Ygo_Earnings(BaseAcs5, GeoId, AcsOccId):
 
     @classmethod
     def get_supported_levels(cls):
-        return {"geo": [NATION, COUNTY, MSA, PLACE, PUMA, ALL], "acs_occ": AcsOccId.LEVELS}
+        return {"geo": [NATION, STATE, COUNTY, MSA, PLACE, PUMA, ALL], "acs_occ": AcsOccId.LEVELS}
 
 
 class Acs3_Ygi_Num_Emp(BaseAcs3, AcsIndId, GeoId):
@@ -88,6 +88,21 @@ class Acs3_Ygi_Num_Emp(BaseAcs3, AcsIndId, GeoId):
         return {"geo": [NATION, STATE, MSA, ALL], "acs_ind": AcsIndId.LEVELS}
 
 
+class Acs5_Ygi_Num_Emp(BaseAcs5, AcsIndId, GeoId):
+    __tablename__ = "ygi_num_emp"
+    median_moe = 1.9
+
+    year = db.Column(db.Integer, primary_key=True)
+    num_emp = db.Column(db.Float)
+    num_emp_moe = db.Column(db.Float)
+    num_emp_rca = db.Column(db.Float)
+
+    @classmethod
+    def get_supported_levels(cls):
+        return {"geo": [NATION, STATE, COUNTY, ALL],
+                "acs_ind": ["0", "1", ALL]}
+
+
 class Acs3_Ygi_MedEarnings(BaseAcs3, AcsIndId, GeoId):
     __tablename__ = "ygi_med_earnings"
     median_moe = 2
@@ -98,4 +113,4 @@ class Acs3_Ygi_MedEarnings(BaseAcs3, AcsIndId, GeoId):
 
     @classmethod
     def get_supported_levels(cls):
-        return {"geo": [NATION, STATE, MSA, ALL], "acs_ind": ["0", "1", "all"]}
+        return {"geo": [NATION, STATE, COUNTY, MSA, ALL], "acs_ind": ["0", "1", "all"]}
