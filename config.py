b9fa11c38acae62d8c99fe1f9dbf31feffd27344
jspeis <jspeis@gmail.com>
2015-11-09 10:26:32 -0500

    adding path to search index dir

--- config.py
+++ config.py
@@ -33,3 +33,4 @@ CACHE_DIR = os.path.join(basedir, 'cache/')
 CACHE_DEFAULT_TIMEOUT = os.environ.get("CACHE_DEFAULT_TIMEOUT", (60 * 60))
 
 FLICKR_DIR = os.environ.get("DATAUSA_FLICKR_DIR", os.path.join(basedir, '../datausa-site/datausa/static/img/splash'))
+SEARCH_INDEX_DIR = os.path.join(basedir, 'search_index/')
