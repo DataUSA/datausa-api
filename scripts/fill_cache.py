803d80dc642120aae867d9ab33f7249a35fc84bc
jspeis <jspeis@gmail.com>
2015-11-11 08:42:43 -0500

    script to populate cache

---
+++ scripts/fill_cache.py
@@ -0,0 +1,49 @@
+import click
+import requests
+import json
+import time
+import threading
+from multiprocessing import Pool
+
+
+def url_to_json(url):
+    print url
+    result = requests.get(url).json()
+    if 'data' in result:
+        return result['data'], result['headers']
+    raise Exception("No data!")
+
+def crawl_page(moi):
+    display_id,attr_kind = moi
+    page = 'http://postgres.datawheel.us:81/profile/{}/{}/'.format( attr_kind, display_id)
+    print page, "getting..."
+    r = requests.get(page)
+    print r.status_code
+
+def crawl_attr(base_url, attr_kind='country'):
+    data,headers = url_to_json('{}/attrs/search?q=&kind={}'.format(base_url, attr_kind))
+    data = sorted(data, key=lambda obj: obj[headers.index('zvalue')], reverse=True)
+    mydata = [[country[headers.index('id')], attr_kind] for country in data]
+    pool = Pool(5)
+    pool.map(crawl_page, mydata)
+
+@click.command()
+@click.argument('base_url', type=str)
+def main(base_url):
+    if not base_url.startswith('http://'):
+        base_url = 'http://' + base_url
+    if base_url.endswith('/'):
+        base_url = base_url[:-1]
+    attrs = ['geo']
+    thread_list = []
+    for attr in attrs:
+        thread = threading.Thread(target=crawl_attr, args=[base_url, attr])
+        thread.start()
+        thread_list.append(thread)
+    print "Waiting for crawl to complete..."
+    for thread in thread_list:
+        thread.join()
+    print "Crawl complete!"
+
+if __name__ == "__main__":
+    main()
