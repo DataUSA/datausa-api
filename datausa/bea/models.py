c275a8dfde3687a516a99553f7d9dd3992aaef67
jspeis <jspeis@gmail.com>
2015-11-16 13:02:41 -0500

    excluding more iocodes

--- datausa/bea/models.py
+++ datausa/bea/models.py
@@ -9,8 +9,10 @@ class BeaUse(db.Model, BaseModel):
     __tablename__ = 'use'
     source_title = 'Bureau of Economic Analysis'
     median_moe = 2
-    to_filter = ["Other", "PROF", "Used", "V001", "V002", "V003", "TOTCOMOUT", "TOTFU", "TOTII", "TOTINDOUT", "TOTVA"]
-
+    to_filter = ["F07S", "TOTCOMOUT", "PROF", "F010", "F07C", "F02S", "HS", "F06N", "F02E", "F07E", "F10N", "F06E",
+                 "F10C","F030", "F10E", "TOTFU", "FIRE", "F050", "F06S", "ORE", "F02R", "F040", "F06C", "GFGD", "F020",
+                 "G", "F07N","F02N", "TOTII", "F10S", "F100", "GFGN", "GSLE", "GFE", "GSLG", "Other", "Used",
+                 "V001", "V002", "V003", "TOTCOMOUT", "TOTFU", "TOTII", "TOTINDOUT", "TOTVA"]
     year = db.Column(db.Integer, primary_key=True)
     industry_iocode = db.Column(db.String, db.ForeignKey(IoCode.id), primary_key=True)
     commodity_iocode = db.Column(db.String, db.ForeignKey(IoCode.id), primary_key=True)
