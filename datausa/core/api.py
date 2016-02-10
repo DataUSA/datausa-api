cf81768ab8abb640f140ce41588534b53847a95a
jspeis <jspeis@gmail.com>
2016-02-10 12:41:35 -0500

    fix for str

--- datausa/core/api.py
+++ datausa/core/api.py
@@ -58,7 +58,7 @@ def parse_method_and_val(cond):
     elif cond.endswith("~$"):
         return "endswith", cond[:-2], True
     elif cond.startswith("str!"):
-        return "ne", str(cond[1:]), False
+        return "ne", str(cond[4:]), False
     elif cond.startswith("!"):
         return "ne", int(cond[1:]), False
     elif cond.startswith(">"):
