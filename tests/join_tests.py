5b2afa2397791d4e7a6b6ec1a3dcc9df66c02c0e
jspeis <jspeis@gmail.com>
2016-08-29 14:31:50 -0400

    updating tests

--- tests/join_tests.py
+++ tests/join_tests.py
@@ -9,20 +9,8 @@ class JoinAPITestCases(unittest.TestCase):
     def setUp(self):
         self.app = datausa.app.test_client()
 
-    def test_no_crosswalk(self):
-        req = self.app.get('/api/join/?required=adult_obesity,income&sumlevel=all&show=geo&geo=16000US2507000')
-        result = json.loads(req.data)
-        assert 'data' in result
-        data = result['data']
-        headers = result['headers']
-        target_index = headers.index('chr.yg.adult_obesity')
-        acs_income_index = headers.index('acs_5yr.yg.income')
-        first_row = data[0]
-        assert not first_row[target_index]
-        assert first_row[acs_income_index] > 0
-
-    def test_auto_crosswalk(self):
-        req = self.app.get('/api/join/?required=adult_obesity,income&sumlevel=all&show=geo&geo=16000US2507000&auto_crosswalk=1')
+    def test_geo_crosswalk(self):
+        req = self.app.get('http://localhost:5000/api/join/?required=adult_obesity,income&sumlevel=all&show=geo&where=income.geo:16000US2507000,adult_obesity.sumlevel:county&year=latest')
         result = json.loads(req.data)
         assert 'data' in result
         data = result['data']
@@ -30,8 +18,22 @@ class JoinAPITestCases(unittest.TestCase):
         target_index = headers.index('chr.yg.adult_obesity')
         chr_geo_index = headers.index('chr.yg.geo')
         first_row = data[0]
+        assert len(data) == 1
         assert first_row[target_index]
         assert first_row[chr_geo_index] == '05000US25025'
+
+    def test_join_but_no_geo_crosswalk(self):
+        req = self.app.get('http://localhost:5000/api/join/?required=pop_black,pop_white,income&sumlevel=all&show=geo&where=income.geo:16000US2511000&year=latest')
+        result = json.loads(req.data)
+        assert 'data' in result
+        data = result['data']
+        headers = result['headers']
+        target_index = headers.index('acs_5yr.yg_race.pop_black')
+        yg_race_geo_index = headers.index('acs_5yr.yg_race.geo')
+        first_row = data[0]
+        assert len(data) == 1
+        assert first_row[target_index]
+        assert first_row[yg_race_geo_index] == '16000US2511000'
 
     def test_display_names(self):
         req = self.app.get('/api/join/?required=adult_obesity,income&sumlevel=all&show=geo&geo=04000US25&display_names=1')
@@ -45,35 +47,11 @@ class JoinAPITestCases(unittest.TestCase):
         assert first_row[target_index] == 'Massachusetts'
 
     def test_limit(self):
-        req = self.app.get('/api/join/?required=grads_total&sumlevel=state&show=geo&limit=3')
+        req = self.app.get('/api/join/?required=grads_total&sumlevel=all&show=geo&limit=3')
         result = json.loads(req.data)
         assert 'data' in result
         data = result['data']
         assert len(data) == 3
-
-    def test_no_subs(self):
-        req = self.app.get('/api/join/?required=adult_obesity,income&sumlevel=all&show=geo&geo=16000US2507000&auto_crosswalk=0')
-        result = json.loads(req.data)
-        assert 'data' in result
-        data = result['data']
-        assert 'subs' in result
-        assert not result['subs']
-
-    def test_subs(self):
-        req = self.app.get('/api/join/?required=adult_obesity,income&sumlevel=all&show=geo&geo=16000US2507000&auto_crosswalk=1')
-        result = json.loads(req.data)
-        assert 'data' in result
-        data = result['data']
-        assert 'subs' in result
-        assert 'chr.yg' in result['subs']
-        assert 'geo' in result['subs']['chr.yg']
-        geo_subs = result['subs']['chr.yg']['geo']
-        assert len(geo_subs) == 1
-        geo_sub = geo_subs[0]
-        assert 'original' in geo_sub
-        assert 'replacement' in geo_sub
-        assert geo_sub['original'] == '16000US2507000'
-        assert geo_sub['replacement'] == '05000US25025'
 
 
 if __name__ == '__main__':
