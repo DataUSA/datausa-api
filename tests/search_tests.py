1f5df454f808f8ac8989b890b6f745a707c257b0
jspeis <jspeis@gmail.com>
2016-10-27 15:05:06 -0400

    moving search tests

---
+++ tests/search_tests.py
@@ -0,0 +1,104 @@
+from whoosh.qparser import QueryParser
+from whoosh import index, sorting, scoring
+from whoosh import qparser, query
+from config import SEARCH_INDEX_DIR
+import math
+import unittest
+from datausa.attrs.views import do_search
+
+ix = index.open_dir(SEARCH_INDEX_DIR)
+qp = QueryParser("name", schema=ix.schema, group=qparser.OrGroup)
+
+facet = sorting.FieldFacet("zvalue", reverse=True)
+scores = sorting.ScoreFacet()
+
+class TestStringMethods(unittest.TestCase):
+  NY_IDS = ['31000US35620', '05000US36061', '04000US36', '16000US3651000']
+
+  def test_extra_word(self):
+      data,suggs,tries,my_vars = do_search("new york economy")
+      self.assertTrue(data[0][0] in self.NY_IDS)
+
+  def test_manhattan(self):
+      data,suggs,tries,my_vars = do_search("manhattan")
+      self.assertEqual(data[0][0], "05000US36061")
+
+  def test_exact_match_begin(self):
+      data,suggs,tries,my_vars = do_search("nome")
+      self.assertEqual(data[0][0], '16000US0254920')
+
+  def test_ny(self):
+      data,suggs,tries,my_vars = do_search("new york")
+      self.assertTrue(data[0][0] in self.NY_IDS)
+
+  def test_doc(self):
+      data,suggs,tries,my_vars = do_search("doctor")
+      self.assertEqual(data[0][0], '291060')
+
+  def test_stl(self):
+      data,suggs,tries,my_vars = do_search("st louis")
+      self.assertEqual(data[0][0], '16000US2965000')
+
+  def test_fortla(self):
+        data,suggs,tries,my_vars = do_search("fort la")
+        self.assertEqual(data[0][0], '16000US1224000')
+
+  def test_bad_spelling(self):
+        data,suggs,tries,my_vars = do_search("massachusitt")
+        self.assertEqual(data[0][0], '04000US25')
+
+  def test_econ(self):
+        econs = ['193011', '450601', '01000US']
+        data,suggs,tries,my_vars = do_search("econ")
+        self.assertTrue(data[0][0] in econs)
+
+  def test_milford(self):
+        data,suggs,tries,my_vars = do_search("milford nh")
+        self.assertEqual(data[0][0], '16000US3347940')
+
+  def test_bevhills(self):
+        data,suggs,tries,my_vars = do_search("beverly hills")
+        self.assertEqual(data[0][0], '16000US0606308')
+
+  def test_kind_naics(self):
+        data,suggs,tries,my_vars = do_search("educat", kind="naics")
+        self.assertTrue(data[0][0])
+
+  def test_ma(self):
+        data,suggs,tries,my_vars = do_search("ma")
+        self.assertEqual(data[0][0], '04000US25')
+
+  def test_ak(self):
+        data,suggs,tries,my_vars = do_search("ak")
+        self.assertEqual(data[0][0], '04000US02')
+
+  def test_pa(self):
+        data,suggs,tries,my_vars = do_search("pa")
+        self.assertEqual(data[0][0], '04000US42')
+
+  def test_al(self):
+        data,suggs,tries,my_vars = do_search("al")
+        self.assertEqual(data[0][0], '04000US01')
+
+  def test_dc(self):
+        data,suggs,tries,my_vars = do_search("dc")
+        self.assertEqual(data[0][0], '16000US1150000')
+
+  def test_rny(self):
+        data,suggs,tries,my_vars = do_search("rochester, ny")
+        self.assertEqual(data[0][0], '16000US3663000')
+
+  def test_cpmd(self):
+        data,suggs,tries,my_vars = do_search("college park, md")
+        self.assertEqual(data[0][0], '16000US2418750')
+
+  def test_moco(self):
+        data,suggs,tries,my_vars = do_search("montgomery county")
+        self.assertEqual(data[0][0], '05000US24031')
+
+  def test_pgc(self):
+        data,suggs,tries,my_vars = do_search("prince georges county")
+        self.assertEqual(data[0][0], '05000US24033')
+
+if __name__ == '__main__':
+    unittest.main()
